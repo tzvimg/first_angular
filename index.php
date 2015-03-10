f75a2df20c8022a79bda260c68734d82fda15cc8
tzvimg <tzvimg@gmail.com>
2015-03-10 16:16:20 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <script src="angular-1.4.0-beta.5/angular.min.js"></script>
+
+  </head>
+  <body>
+<h1>Sample Application</h1>
+<div ng-app="">
+   <p>Enter your Name: <input type="text" ng-model="name"></p>
+   <p>Hello <span ng-bind="name"></span>!</p>
+</div>
+    </div>
+  </body>
+</html>
